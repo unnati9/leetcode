16342b50665ae34157677b48acbd85ed6b75b7aa
Unnati Patil <unnatipatil900@gmail.com>
2025-01-22 21:06:38 +0530

    Time: 5 ms (97.97%) | Memory: 49.5 MB (99.45%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false;
+    let count = new Array(26).fill(0);
+    for (let i = 0; i < s.length; i++) {
+        count[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+        count[t.charCodeAt(i) - 'a'.charCodeAt(0)]--;
+    }
+    for (let i = 0; i < 26; i++) {
+        if (count[i] !== 0) return false;
+    }
+    return true;
+};
